0aafa8b4919fbed73d2acd03d50ec4b4060c9044
Can Kesimliler <cankesimliler@rockbit.pete.metu.edu.tr>
2024-04-24 23:05:49 +0300

    v1.1

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const clock = document.getElementById("clock");
 setInterval(() => {
   const date = new Date();
-  const hours = date.getHours().toString().padStart(2, 0);
+  let hours = date.getHours();
+  const meridiem = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  hours = hours.toString().padStart(2, 0);
   const seconds = date.getMinutes().toString().padStart(2, 0);
   const minutes = date.getSeconds().toString().padStart(2, 0);
-  const time = `${hours}:${seconds}:${minutes}`;
+  const time = `${hours}:${seconds}:${minutes} ${meridiem}`;
   clock.textContent = time;
 }, 1000);
